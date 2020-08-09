cbce0f67a4b3b2d2736262f2c726d585d9bbd0cf
Thanyla Sales <thanylas@gmail.com>
2020-08-08 23:32:29 -0300

    Close issue-225

--- mobile/app/componentes/ModalFormNotificacao.js
+++ mobile/app/componentes/ModalFormNotificacao.js
@@ -6,70 +6,80 @@ import {
     TextInput,
     TouchableHighlight,
     TouchableOpacity,
+    TouchableWithoutFeedback,
     View,
+    Picker,
 } from 'react-native';
 import { enviarNotificacaoAglomeracao } from '../utils/LocalizacaoDispositivo';
 
-export default function ModalFormNotificacao({ modalVisible, closeModal}){
+export default function ModalFormNotificacao({ modalVisible, closeModal }) {
     const [observacao, setObservacao] = useState('')
     const [estimativa, setEstimativa] = useState('')
-
+    const [estimativaPicker, setEstimativaPicker] = useState('');
     return (
         <View style={styles.centeredView}>
             <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-            }}
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                }}
             >
                 <TouchableOpacity
-                    style={styles.container}
+                    style={styles.containerTouchable}
                     activeOpacity={1}
                     onPressOut={() => {
                         closeModal()
+                        setEstimativaPicker('');
+                        setObservacao('');
                     }}
                 >
-                <View style={styles.centeredView}>
-                    <View style={styles.modalView}>
+                    <TouchableWithoutFeedback onPress={() => { }}>
+                        <View style={styles.modalView}>
                         <Text style={styles.label}>Estimativa de pessoas</Text>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Número de pessoas no local"
-                            numeric
-                            keyboardType={'numeric'}
-                            onChangeText={number => setEstimativa(number)}
-                        />
-                        <Text style={styles.label}>Observação</Text>
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="(Opicional)"
-                            multiline={true}
-                            value={observacao} 
-                            onChangeText={text => setObservacao(text)}/>
+                            <View style={styles.inputPicker}>
+                                <Picker
+                                    selectedValue={estimativaPicker}
+                                    onValueChange={value => setEstimativaPicker(value)}>
+                                    <Picker.Item label="0 a 9" value="0" />
+                                    <Picker.Item label="10 a 19" value="10" />
+                                    <Picker.Item label="20 a 39" value="20" />
+                                    <Picker.Item label="40 a 79" value="40" />
+                                    <Picker.Item label="80 a 99" value="80" />
+                                    <Picker.Item label="100 a 999" value="100" />
+                                    <Picker.Item label="1000 ou mais" value="1000" />
+                                </Picker>
+                            </View>
+                            <Text style={styles.label}>Observação</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder="(Opicional)"
+                                multiline={true}
+                                value={observacao}
+                                onChangeText={text => setObservacao(text)} />
 
-                        <TouchableHighlight
-                            style={{ ...styles.openButton, backgroundColor: '#94D451' }}
-                            onPress={() => {
-                                if(estimativa != ''){
-                                    const enviarNotificacao = async () => {
-                                        try {
-                                                await enviarNotificacaoAglomeracao(estimativa, observacao);
-                                        } catch (error) {
-                                            console.log(`Error ao enviar notificação: ${error}`);
+                            <TouchableHighlight
+                                style={{ ...styles.openButton, backgroundColor: '#94D451' }}
+                                onPress={() => {
+                                    if (estimativa != '') {
+                                        const enviarNotificacao = async () => {
+                                            try {
+                                                await enviarNotificacaoAglomeracao(estimativaPicker, observacao);
+                                            } catch (error) {
+                                                console.log(`Error ao enviar notificação: ${error}`);
+                                            }
                                         }
+                                        enviarNotificacao();
                                     }
-                                    enviarNotificacao();
-                                }
-                                setObservacao('')
-                                setEstimativa('')
-                                closeModal()
-                            }}
-                        >
-                            <Text style={styles.textStyle}>Enviar notificação</Text>
-                        </TouchableHighlight>
-                    </View>
-                </View>
+                                    setObservacao('')
+                                    setEstimativaPicker('')
+                                    closeModal()
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Enviar notificação</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </TouchableWithoutFeedback>
                 </TouchableOpacity>
             </Modal>
         </View>
@@ -77,11 +87,16 @@ export default function ModalFormNotificacao({ modalVisible, closeModal}){
 };
 
 const styles = StyleSheet.create({
+    containerTouchable: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     label: {
         alignSelf: 'flex-start',
         color: '#326002',
         fontWeight: 'bold',
-        marginTop: 5, 
+        marginTop: 5,
     },
     input: {
         backgroundColor: '#eee',
@@ -91,16 +106,12 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 20,
     },
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 22,
+    inputPicker:{
+        backgroundColor: '#eee',
+        alignSelf: 'stretch',
+        textAlign: 'left',
+        margin: 10,
+        borderRadius: 20,
     },
     modalView: {
         margin: 20,
@@ -110,9 +121,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         shadowColor: '#000',
         shadowOffset: {
-        width: 0,
-        height: 2,
-    },
+            width: 0,
+            height: 2,
+        },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 10,
